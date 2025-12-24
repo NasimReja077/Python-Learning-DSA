e051ad45aa753977f3ce0f467c114687f4b998ae
Nasim <nasim.reja.077@gmail.com>
2025-12-24 23:55:44 +0530

    Add

--- DSA/Singly-LinkedList/singlylinkedlist.py
+++ DSA/Singly-LinkedList/singlylinkedlist.py
@@ -21,33 +21,33 @@ class SinglyLinkedList:
 
     # ii) Insert a node at the end
     def insert_end(self, value):
-        new_node = Node(value)
+        new_node = Node(value) # Create a new node with given value
 
         if self.head is None:
-            self.head = new_node
+            self.head = new_node # If list is empty, new node becomes head, 
             print(value, "inserted at end")
             return
 
-        temp = self.head
+        temp = self.head # Start from head node 
         while temp.next:
-            temp = temp.next
+            temp = temp.next # Traverse to the last node, and then link new node to it 
 
-        temp.next = new_node
+        temp.next = new_node # Link the last node to new node, and new node points to None
         print(value, "inserted at end")
 
 
     # iii) Insert a node in between (after a given value)
     def insert_after(self, prev_value, value):
-        temp = self.head
+        temp = self.head # temp is used to traverse the list
 
         while temp:
-            if temp.data == prev_value:
-                new_node = Node(value)
-                new_node.next = temp.next
-                temp.next = new_node
+            if temp.data == prev_value: # Found the previous value, temp.data is meaning the data of current node
+                new_node = Node(value) # Create a new node with given value
+                new_node.next = temp.next # Link new node to the next of current node, new_node.next means the next of new node , new_node.next = temp.next means the next of new node is linked to the next of current node
+                temp.next = new_node # Link current node to new node
                 print(value, "inserted after", prev_value)
                 return
-            temp = temp.next
+            temp = temp.next # Move to the next node
 
         print("Value", prev_value, "not found")
 
@@ -58,11 +58,11 @@ class SinglyLinkedList:
             print("Linked List is empty")
             return
 
-        temp = self.head
+        temp = self.head # Start from head node, temp means current node, so temp = self.head means current node is head node
         print("Linked List:")
         while temp:
-            print(temp.data, end=" -> ")
-            temp = temp.next
+            print(temp.data, end=" -> ") #temp.data means the data of current node, end=" -> " means print " -> " after printing the data of current node
+            temp = temp.next # Move to the next node
         print("NULL")
 
 
@@ -97,8 +97,8 @@ class SinglyLinkedList:
             print("List is empty")
             return
 
-        print(self.head.data, "deleted from beginning")
-        self.head = self.head.next
+        print(self.head.data, "deleted from beginning") # self.head.data means the data of head node
+        self.head = self.head.next 
 
 
     # viii) Delete from middle (given element)
@@ -107,15 +107,15 @@ class SinglyLinkedList:
         prev = None
 
         while temp:
-            if temp.data == key:
+            if temp.data == key: # Found the key to be deleted
                 if prev is None:
-                    self.head = temp.next
+                    self.head = temp.next # If head node is to be deleted, update head to next node
                 else:
-                    prev.next = temp.next
+                    prev.next = temp.next 
                 print(key, "deleted from linked list")
                 return
-            prev = temp
-            temp = temp.next
+            prev = temp # Move prev to current node
+            temp = temp.next 
 
         print(key, "not found")
 
