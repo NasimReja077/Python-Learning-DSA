bb6cb110134322cd33900ebdf97b1eec25bd4f6a
Nasim <nasim.reja.077@gmail.com>
2026-05-08 17:28:41 +0530

    feat: enhance Binary Search Tree with improved insert, search, delete methods and add level order traversal

--- DSA/Tree/Binary Search Tree/BST2.py
+++ DSA/Tree/Binary Search Tree/BST2.py
@@ -1,3 +1,7 @@
+from collections import deque
+from logging import root
+
+
 class Node:
     def __init__(self, value):
         self.left = None
@@ -7,65 +11,95 @@ class Node:
 
 # -------- INSERT --------
 def insert(root, value):
+    
+    # If tree empty, create root
     if root is None:
         return Node(value)
-
+    
+    # Duplicate values not allowed
+    if root.data == value:
+        return root
+    
+    # Insert in left subtree
     if value < root.data:
         root.left = insert(root.left, value)
+    # Insert in right subtree
     elif value > root.data:
         root.right = insert(root.right, value)
 
     return root
 
+# time complexity: 
+# - Average Case: O(log n) - when the tree is balanced
+# - Worst Case: O(n) - when the tree is skewed (like a linked list)
+# space complexity: O(h) where h is the height of the tree 
+
 
 # -------- SEARCH --------
 def search(root, value):
+    # Value not found
     if root is None:
-        print("Element not found")
-        return
+        print("Element not found", end="\n")
+        return False
 
+    # Value found
     if root.data == value:
-        print("Element Found")
-        return
+        print("Element Found", end="\n")
+        return True
 
-    elif value < root.data:
-        search(root.left, value)
-    else:
-        search(root.right, value)
+    # Search in left subtree
+    if value < root.data:
+        return search(root.left, value)
+    
+    else: # Search in right subtree
+        return search(root.right, value)
+
+# time complexity: 
+# - Average Case: O(log n)
+# - Worst Case: O(n) 
+# space complexity: O(h)
 
 
 # -------- GET SUCCESSOR --------
 def get_successor(root):
     root = root.right
-    while root and root.left:
+    while root and root.left is not None:
         root = root.left
     return root
 
 
 # -------- DELETE --------
 def delete(root, value):
-    if root is None:
+    if root is None: #  If Node not found, return None
         return root
-
+    
+    # 1. Search for the node
     if value < root.data:
         root.left = delete(root.left, value)
-
+        
     elif value > root.data:
         root.right = delete(root.right, value)
 
     else:
-        # Case 1 & 2
+        # 2. Node found, handle deletion
+        # Case A & B: Node has one child or no children
         if root.left is None:
             return root.right
+        
         if root.right is None:
             return root.left
 
-        # Case 3
+        # Case 3: Node has two children
+        
         succ = get_successor(root)
-        root.data = succ.data
-        root.right = delete(root.right, succ.data)
-
+        root.data = succ.data # Replace nodes value with successor's value
+        root.right = delete(root.right, succ.data) # Delete the successor node from right subtree
     return root
+
+# time complexity: 
+# - Average Case: O(log n) 
+# - Worst Case: O(n)
+# space complexity: O(h) 
 
 
 # -------- TRAVERSALS --------
@@ -76,23 +110,46 @@ def inorder(root):
         print(root.data, end=" ")
         inorder(root.right)
 
-
 def preorder(root):
     if root:
         print(root.data, end=" ")
         preorder(root.left)
         preorder(root.right)
 
-
 def postorder(root):
     if root:
         postorder(root.left)
         postorder(root.right)
         print(root.data, end=" ")
+        
+# Level Order Traversal
+def level_order(root):
+    if root is None:
+        print("Tree is empty")
+        return
+    
+    queue = deque([root])
+    print("Level Order Traversal:", end=" ")
+    while queue:
+        current = queue.popleft()
+        print(current.data, end=" ")
+        if current.left:
+            queue.append(current.left)
+        if current.right:
+            queue.append(current.right)
+            
+    print()
 
+# time complexity: 
+# - Inorder, Preorder, Postorder: o(n) - we visit each node one time
+# - Level Order: O(n) - we visit each node one time
+# Space Complexity: O(h) for recursive traversals, O(n) for level order traversal in worst case (when tree is completely unbalanced)
 
 # -------- FIND MIN --------
 def find_min(root):
+    if root is None:
+        print("Tree is empty")
+        return None
     while root.left:
         root = root.left
     return root.data
@@ -100,6 +157,9 @@ def find_min(root):
 
 # -------- FIND MAX --------
 def find_max(root):
+    if root is None:
+        print("Tree is empty")
+        return None
     while root.right:
         root = root.right
     return root.data
@@ -108,29 +168,59 @@ def find_max(root):
 # -------- FIND HEIGHT --------
 def height(root):
     if root is None:
-        return -1   # height in edges
+        return -1   # height in edges # why -1? empty tree has height -1, single node tree has height 0
     return 1 + max(height(root.left), height(root.right))
 
+# -------- FIND HEIGHT IN NODES --------
+def height_in_nodes(root):
+    if root is None:
+        return 0   # height in nodes
+    return 1 + max(height_in_nodes(root.left), height_in_nodes(root.right))
 
 # -------- DRIVER --------
-root = None
-values = [20, 15, 30, 40, 12, 18, 25, 50]
+# root = None
+# values = [20, 15, 30, 40, 12, 18, 25, 50]
 
-for v in values:
-    root = insert(root, v)
+# for v in values:
+#     root = insert(root, v)
 
-print("Inorder:", end=" ")
+root = insert(None, 20)
+root = insert(root, 15)
+root = insert(root, 30)
+root = insert(root, 40)
+root = insert(root, 12)
+root = insert(root, 18)
+root = insert(root, 25)
+root = insert(root, 50)
+
+
+print("Inorder Traversal :", end=" ")
 inorder(root)
+print()
 
-print("\nPreorder:", end=" ")
+
+print("Preorder Traversal :", end=" ")
 preorder(root)
+print()
 
-print("\nPostorder:", end=" ")
+print("Postorder Traversal :", end=" ")
 postorder(root)
+print()
 
-print("\nMin:", find_min(root))
-print("Max:", find_max(root))
-print("Height:", height(root))
+print("Level Order Traversal :", end=" ")
+level_order(root)
+
+print("\nMinimum Value :", find_min(root))
+print("Maximum Value :", find_max(root))
+print("Height (edges) :", height(root))
+print("Height (nodes) :", height_in_nodes(root))
 
 search(root, 18)
 search(root, 100)
+
+    # Test Deletion
+print("\nDeleting 30...")
+root = delete(root, 30)
+print("Inorder after deletion:", end=" ")
+inorder(root)
+print()
