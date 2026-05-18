2f5fda9c5d24e36cc1f1c7d72da838329307b0a6
Nasim <nasim.reja.077@gmail.com>
2026-05-18 22:10:58 +0530

    fix: enhance comments for clarity in QuickSort and partition functions

--- DSA/Sorting/QuickSort/QuickSort.py
+++ DSA/Sorting/QuickSort/QuickSort.py
@@ -6,25 +6,30 @@ def QuickSort(arr, l, r):
         QuickSort(arr, p+1, r)
 
 def partition(arr, l, r):
-    pivot = arr[l]
-    i = l + 1
-    j = r
+    pivot = arr[l] # Selects the first element as pivot (common but not the safest choice).
+    i = l + 1 #  Left pointer starts just after pivot.
+    j = r # Right pointer starts at the end
 
     while True:
-        while( i <= j and arr[i] < pivot):
+        while( i <= j and arr[i] < pivot): # Move i to the right until we find an element ≥ pivot (or i > j).
             i = i + 1
         
-        while( i <= j and arr[j] > pivot):
+        while( i <= j and arr[j] > pivot): # Move j to the left until we find an element ≤ pivot (or i > j).
             j = j - 1
         
+        # If i is still left of j, swap the two misplaced elements.
+        # If i >= j, pointers have crossed → break the loop.
+
         if( i < j ):
-            arr[i], arr[j] = arr[j], arr[i]
+            # Swap arr[i] and arr[j] to correct their positions relative to the pivot.
+            arr[i], arr[j] = arr[j], arr[i] 
         else:
             break
     
-    arr[l], arr[j] = arr[j], arr[l]
+    arr[l], arr[j] = arr[j], arr[l] # Finally, swap the pivot (at position l) with element at j.
+    # why l and j ? -> l is the index of pivot and j is the index of last element in left partition. So we will swap pivot with last element in left partition. So pivot will be in correct position.
     return j
 
 arr = [23, 45, 12, 65, 34, 10, 3]
 QuickSort(arr, 0, len(arr)-1)
-print(arr)
+print("QuickSort: ", arr)
